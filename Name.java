c4bf2011aa205f6842d8cb41b2c973e54213575e
Shilpa Bhanot <109947821+shilpabhanot@users.noreply.github.com>
2022-07-26 13:37:22 +0530

    Test Submitted

---
+++ Name.java
@@ -0,0 +1,82 @@
+//Question 3:1)Define .javafile and .class in java.
+//Answer) .javafile will compile the program and shows the error which are occur in our program when we compile it.and .class will compile and run the program.
+
+
+//2) Explain if java is a complete OOPs programming language or not.
+//Answer) Java is not a complete oops programming language because it uses the primitive data types like int, long, float, double, char etc.
+//so it is not a complete oops program language .it is not always uses the objects.
+
+
+//3)Give example of NR-WA (no return with parameter
+/* void testMe(int a) {
+System.out.println("value of variable a="+a);
+}  */
+
+
+// Question 5)Wrapper classes
+//wrapper classes are those classes in which we use capital letter for the data type.
+//example of wrapper class 
+//Integer a2=20;
+
+//Question 2: .class name=MyWorld
+//.function name=myWorld
+//.variable=myWorld
+//Static variable=MY_WORLD
+
+
+//Question 1  
+class Name {
+	int name;
+	int age;
+public static void main(String args[]) {
+	System.out.println("shilpa");
+	System.out.println("age");
+
+
+hobby h=new hobby();
+h.hobby();
+}
+void hobby() {
+	
+	System.out.println("my hobby is drawing");
+}
+
+
+
+}
+//Question 4
+class calci{
+	int a=10;
+	int d=20;
+	static int b=30;
+	static int c=40;
+	
+public static void main(String args[])  {
+	calci obj=new calci();
+	calci.addy();
+	calci.subby();
+	calci.diviji();
+	obj.mull();
+}
+static int addy() {
+	System.out.println(b);
+	
+	return 0;
+	static int subby() {
+		System.out.println(c);
+		return 0;
+	}
+	int diviji() {
+		System.out.println(a+"  "+d);
+		return 0;
+	}
+	int null() {
+		System.out.println(b+"  "+c);
+		return 0;
+	}
+}
+	
+
+	
+	
+
