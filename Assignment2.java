bf2a3e5171a69034692603d91e2bbb4251e3d8eb
Shilpa Bhanot <109947821+shilpabhanot@users.noreply.github.com>
2022-08-02 14:04:02 +0530

    assignment2

---
+++ Assignment2.java
@@ -0,0 +1,135 @@
+//Question 1 
+/*import java.util.Scanner;
+class Assignment2 {
+public static void main(String args[]) {
+	int number;
+Scanner sc=new Scanner(System.in);
+System.out.print("enter the number");
+number=sc.nextInt();
+	if (number%2==0) {
+		System.out.println(number+"is even");
+	}
+		else {
+			System.out.println(number+"is odd");
+}
+}
+} */
+
+
+//Question 6
+
+/*import java.util.Scanner;
+class Assignment2 {
+	public static void main(String args[]) { 
+	int number=0;
+	Scannner r=new  Scanner(System.in);
+	int number=r.nextInt();
+	for (int i=1;i<=8;i++) {
+		System.out.print(i);
+	}
+	}
+} */
+//Question 4
+/*import java.util.Scanner;
+class Assignment2 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		
+		switch (i) {
+			case 579:
+		System.out.println("sonu rollnumber");
+		break;
+		case 546:
+		System.out.println("monu roll number");
+		break;
+		case 553:
+		System.out.println("manu roll number");
+		break;
+		case 554:
+		System.out.println("meenu roll number");
+		break;
+		case 516:
+		System.out.println("meena roll number");
+		break;
+		default:
+		System.out.println("message not allowed");
+		}
+	}
+} */
+		//Question 5
+		//Pass by Value
+		//It is a process in which the functions parameters are copied to another variable and instead of the object passing the copy  is passed.this is called pass by value.
+		
+		//Pass by reference 
+		//it is a process in which the actual reference is passed to the function.this is called pass by reference
+		
+//question 8
+/*class Assignment2 {
+	public static void main(String args[]) {
+		for(int i=1;i<=3;i++) {
+			int num;
+		
+				if (i%2==0) {
+					
+				num=0;
+		System.out.print(num);
+				
+			}
+			else {
+				num=1;
+				System.out.print(num);
+			}
+			for (int j=1;j<=4;j++) {
+				if (num==1) {
+					num=0;
+					System.out.print(num);
+				}
+				else {
+					num=1;
+					System.out.print(num);
+				}
+			}
+			System.out.println();
+		}
+	} 
+} */
+
+//Question 7
+/*import java.util.Scanner;
+class Assignment2 {
+	public static void main (String args[]) {
+		int m;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number of elements");
+		m=sc.nextInt();
+		
+		int[]array=new int[10];
+		System.out.print("enter number of elements");
+		for (int i=0;i<m;i++) {
+			array[i]=sc.nextInt();
+		}
+	System.out.print("enter array elements");
+	for (int i=0;i<m;i++){
+		System.out.println(array[i]);
+	}
+	}
+} */
+		
+		
+
+
+
+
+
+				
+			
+		
+
+	
+	
+
+
+
+
+
